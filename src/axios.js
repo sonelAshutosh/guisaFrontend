aab0ecf40bf02194fff1df8f023f55479adc9b90
Ashutosh Sonel <ashutoshhoonmain@gmail.com>
2024-09-29 15:39:13 +0530

    Added the server url

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: 'http://localhost:5500/',
-  // baseURL: 'https://guisa-backend.vercel.app',
+  // baseURL: 'http://localhost:5500/',
+  baseURL: 'https://guisa-backend.vercel.app',
 })
 
 export default API
